4a9f13674c8029ead171c659ce491252f17b6fab
Aakash Baskaran <aakashguru6898@gmail.com>
2025-08-06 18:39:43 -0400

    Splash screen is created and app icon is changed

---
+++ app/src/main/java/com/aakash/qrcraft/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.aakash.qrcraft
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.aakash.qrcraft.ui.theme.QRCraftTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    private var keepSplashScreen = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+// ✅ Show the splash screen and keep it based on a condition
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QRCraftTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000L) // 5 seconds in debug mode
+            keepSplashScreen = false
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QRCraftTheme {
+        Greeting("Android")
+    }
+}
